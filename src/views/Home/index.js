bc2b3575e6918ca1ee85bfda0ea73fcbda074b84
Joe Cuffney <Joe.Cuffney@nike.com>
2018-08-30 22:10:04 -0700

    fixed nav issue at homepage

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import Nav from '../../components/Nav';
 
 import './Home.css';
 
-const Home = () => (
-  <div className="home_wrapper">
-    <div className="home_flex-wrapper">
-      <div className="home_flex-item">
-        <img className="home_avatar" src="/imgs/avatar.jpg" alt="" />
-        <h1 className="home_masthead">Joe Cuffney.</h1>
-        <h3 className="home_subheader">Sr. Software Engineer.</h3>
-        <Nav />
+const Home = (props) => {
+  const { location: { pathname } } = props;
+  return (
+    <div className="home_wrapper">
+      <div className="home_flex-wrapper">
+        <div className="home_flex-item">
+          <img className="home_avatar" src="/imgs/avatar.jpg" alt="" />
+          <h1 className="home_masthead">Joe Cuffney.</h1>
+          <h3 className="home_subheader">Sr. Software Engineer.</h3>
+          <Nav active={pathname} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+Home.propTypes = {
+  location: propTypes.string.isRequired,
+};
 
 export default Home;
