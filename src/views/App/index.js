420fd0d1f4b8ce95b4953f32b769cfb0c185399f
Joe Cuffney <Joe.Cuffney@nike.com>
2018-08-30 20:25:40 -0700

    cleaned up homepage

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import HomeView from '../Home';
+import Home from '../Home';
 import Work from '../Work';
-import Contact from '../Contact';
 import NotFound from '../NotFound';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={HomeView} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/work" component={Work} />
-      <Route exact path="/contact" component={Contact} />
       <Route path="*" component={NotFound} />
     </Switch>
   </Router>
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,66 +1,30 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Container } from 'semantic-ui-react';
+// import { Link } from 'react-router-dom';
 
-// import { Header as SemanticHeader } from 'semantic-ui-react';
-import Header from '../../components/Header';
-import ResponsiveContainer from '../../components/ResponsiveContainer';
-import getPage from '../../actions/page/action';
-import MastheadSection from '../../components/MastheadSection';
-import ImageSection from '../../components/ImageSection';
-import TextSection from '../../components/TextSection';
-import GridItemsSection from '../../components/GridItemsSection';
+import './Home.css';
 
-export class Home extends PureComponent {
-  componentWillMount() {
-    const { getPageBySlug } = this.props;
-    getPageBySlug('/');
-  }
-
-  static renderSections(sections) {
-    return sections.map((section) => {
-      const { id, name } = section;
-      switch (name) {
-        case 'masthead':
-          return <MastheadSection {...section} />;
-        case 'text':
-          return <TextSection key={id} {...section} />;
-        case 'grid-items':
-          return <GridItemsSection key={id} {...section} />;
-        case 'image':
-          return <ImageSection {...section} />;
-        default:
-          return null;
-      }
-    });
-  }
-
-  render() {
-    const { sections } = this.props;
-    return (
-      <ResponsiveContainer>
-        <Header />
-        { Home.renderSections(sections) }
-      </ResponsiveContainer>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <Container fluid className="home_wrapper">
+      <div className="home_flex-wrapper">
+        <div className="home_flex-item">
+          <img className="home_avatar" src="/imgs/avatar.jpg" alt="" />
+          <h1 className="home_masthead">Joe Cuffney</h1>
+          <h3 className="home_subheader">Sr. Software Engineer.</h3>
+          {/* <Link className="home_nav-link" to="/work">See my Work.</Link> */}
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 
 Home.propTypes = {
-  sections: PropTypes.arrayOf(PropTypes.object),
-  getPageBySlug: PropTypes.func,
 };
 
 Home.defaultProps = {
-  sections: [],
-  getPageBySlug: () => [],
 };
 
-const mapStateToProps = state => ({ sections: state.page.sections });
-
-const mapDispatchToProps = dispatch => ({
-  getPageBySlug: () => dispatch(getPage()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
