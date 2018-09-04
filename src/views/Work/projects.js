a7ac8e25a5579f23f413d25d134ee9fb61fa1dbb
Joe Cuffney <Joe.Cuffney@nike.com>
2018-09-03 21:26:37 -0700

    added projects to work page

---
+++ src/views/Work/projects.js
@@ -0,0 +1,42 @@
+const projects = [
+  {
+    src: '/imgs/work/tandem.png',
+    title: 'Tandem Personal Training',
+    to: 'https://tandempersonaltraining.com',
+  },
+  {
+    src: '/imgs/work/cultivator.jpg',
+    title: 'Cultivator',
+    to: '/internal',
+  },
+  {
+    src: '/imgs/work/klamath.jpg',
+    title: 'Discover Klamath',
+    to: 'https://discoverklamath.com',
+  },
+  {
+    src: '/imgs/work/eyn.jpg',
+    title: 'Earn Your Name',
+    to: 'http://earnyour.name',
+  },
+  {
+    src: '/imgs/work/treehaus.jpg',
+    title: 'Tree Haus',
+  },
+  {
+    src: '/imgs/work/ledbylight.jpg',
+    title: 'Led Lenser | Led By Light',
+    to: 'https://www.ledlenserusa.com/ledbylight.html',
+  },
+  {
+    src: 'http://placehold.it/400x250',
+    title: 'AWS Certification',
+  },
+  {
+    src: 'http://placehold.it/400x250',
+    title: 'Wasserman',
+    to: 'http://www.teamwass.com/',
+  },
+];
+
+module.exports = { projects };
