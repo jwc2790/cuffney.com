187eacd9aae3d9716b7fcf776f42fa5cc931162e
Joe Cuffney <jcuffney@Joes-MacBook-Pro.local>
2019-02-24 14:47:30 -0800

    fixed test

--- src/views/Project/Project.test.js
+++ src/views/Project/Project.test.js
@@ -3,7 +3,14 @@ import { shallow } from 'enzyme';
 import Project from './index';
 
 describe('Work', () => {
+  let match;
+  beforeEach(() => {
+    match = {
+      params: { slug: 'slug' },
+    };
+  });
+
   it('renders as expected', () => {
-    shallow(<Project location={{ pathname: '/work/test' }} />);
+    shallow(<Project location={{ pathname: '/work/test' }} match={match} />);
   });
 });
