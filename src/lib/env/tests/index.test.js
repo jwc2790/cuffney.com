0bd3cd46afbc534acbdb87e294b14817efd9284a
Joseph Cuffney <josephcuffney@gmail.com>
2019-02-27 15:03:42 -0800

    cleaned up tests

--- src/lib/env/tests/index.test.js
+++ src/lib/env/tests/index.test.js
@@ -1,12 +1,11 @@
 import { isTest, getAnalyticsId } from '..';
 import { assert } from 'chai';
-import sinon from 'sinon';
+// import sinon from 'sinon';
 
 describe('lib/env', () => {
 
-    const env = Object.assign({}, process.env);
     const ANALYTICS_DEV = 'UA-45447105-7';
-    const ANALYTICS_PROD = 'UA-45447105-1';
+    // const ANALYTICS_PROD = 'UA-45447105-1';
 
     beforeEach(() => {
         process.env = Object.assign(process.env, { CUSTOM_VAR: 'value' });
@@ -29,25 +28,25 @@ describe('lib/env', () => {
         })
     });
 
-    // describe('getAnalyticsId', () => {
-    //     it('should return ANALYTICS_DEV for NODE_ENV=test', () => {
-    //         process.env.NODE_ENV = 'test';
-    //         assert.equal(getAnalyticsId(), ANALYTICS_DEV)
-    //     })
+    describe('getAnalyticsId', () => {
+        it('should return ANALYTICS_DEV for NODE_ENV=test', () => {
+            process.env.NODE_ENV = 'test';
+            assert.equal(getAnalyticsId(), ANALYTICS_DEV)
+        })
 
-    //     it('should return ANALYTICS_DEV for NODE_ENV=dev', () => {
-    //         process.env.NODE_ENV = 'dev';
-    //         assert.equal(getAnalyticsId(), ANALYTICS_DEV)
-    //     })
+        it('should return ANALYTICS_DEV for NODE_ENV=dev', () => {
+            process.env.NODE_ENV = 'dev';
+            assert.equal(getAnalyticsId(), ANALYTICS_DEV)
+        })
 
-    //     // TODO: Stub `process.env`.  this solution dosen't seem to be working
-    //     // it('should return ANALYTICS_PROD for NODE_ENV=prod', () => {
-    //     //     process.env.NODE_ENV = 'prod';
-    //     //     assert.equal(getAnalyticsId(), ANALYTICS_PROD)
-    //     // })
+        // TODO: Stub `process.env`.  this solution dosen't seem to be working
+        // it('should return ANALYTICS_PROD for NODE_ENV=prod', () => {
+        //     process.env.NODE_ENV = 'prod';
+        //     assert.equal(getAnalyticsId(), ANALYTICS_PROD)
+        // })
 
-    //     it('should return ANALYTICS_DEV for undefined NODE_ENV', () => {
-    //         assert.equal(getAnalyticsId(), ANALYTICS_DEV)
-    //     })
-    // });
+        it('should return ANALYTICS_DEV for undefined NODE_ENV', () => {
+            assert.equal(getAnalyticsId(), ANALYTICS_DEV)
+        })
+    });
 });
--- src/views/App/tests/App.test.js
+++ src/views/App/tests/App.test.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import withTracker from 'components/WithTracker';
 import App from '../index';
 
 describe('App', () => {
-
-  beforeEach(() => {
-    // mock the HOC withTracker
-  })
 
   it('renders as expected', () => {
     shallow(<App />);
   })
 
-  // TODO:
-  // it('should call withTracker once for the defualt route', () => {})
 });
