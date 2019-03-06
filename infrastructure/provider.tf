854c39ead0cd1c95822bb7680c5059195e4fef24
Joseph Cuffney <josephcuffney@gmail.com>
2019-03-06 13:31:10 -0800

    added remote e3 terraform backend

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,10 +1,15 @@
+# use s3 bucket as a remote backend for terraform
+terraform {
+  backend "s3" {
+    bucket = "com.cuffney.terraform"
+    key    = "cuffney.com/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+  depends_on = "${aws_s3_bucket.terraform_backend_store}"
+}
+
 provider "aws" {
   region  = "us-east-1"
   profile = "cuffney"
-
-  # assume_role {
-  #   role_arn     = "arn:aws:iam::255964265911:user/jcuffney"
-  #   # session_name = "SESSION_NAME"
-  #   # external_id  = "EXTERNAL_ID"
-  # }
 }
