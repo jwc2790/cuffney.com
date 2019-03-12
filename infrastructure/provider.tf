5883651f9f351cbcb104f6397d405371548fea4f
Joseph Cuffney <josephcuffney@gmail.com>
2019-03-12 06:30:00 -0700

    working with s3 backend locally

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,13 +1,12 @@
 # use s3 bucket as a remote backend for terraform
-# terraform {
-#   backend "s3" {
-#     bucket = "com.cuffney.terraform"
-#     key    = "cuffney.com/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-
-#   depends_on = "${aws_s3_bucket.terraform_backend_store}"
-# }
+terraform {
+  backend "s3" {
+    bucket = "com.cuffney.terraform"
+    key    = "cuffney.com/terraform.tfstate"
+    region = "us-east-1"
+    profile = "cuffney"
+  }
+}
 
 provider "aws" {
   region  = "us-east-1"
